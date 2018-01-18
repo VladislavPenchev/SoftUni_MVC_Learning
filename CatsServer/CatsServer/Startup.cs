d30c868d9c1b2883830836e9d9fac439eeb497c6
VladislavPenchev <vgpenchev@gmail.com>
2018-01-18 13:13:07 +0200

    Implement page

--- CatsServer/CatsServer/Startup.cs
+++ CatsServer/CatsServer/Startup.cs
@@ -6,6 +6,8 @@
     using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
+    using System.Linq;
+    using System.Net.Http.Headers;
 
     public class Startup
     {
@@ -17,14 +19,56 @@
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.Use((context, next) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                context.RequestServices.GetRequiredService<CatsDbContext>().Database.Migrate();
+                return next();
+            });
+
+            app.Use((context, next) =>
+            {
+                context.Response.Headers.Add("Content-Type", "text/html");
+                return next();
+            });
+
+            app.MapWhen(
+                ctx => ctx.Request.Path.Value == "/"
+                && ctx.Request.Method == "GET", 
+                home =>
+                {
+                    home.Run(async (context) =>
+                    {
+                        await context.Response.WriteAsync($"<h1>{env.ApplicationName}</h1>");
+
+                        var db = context.RequestServices.GetService<CatsDbContext>();
+
+                        var catLinks = db
+                            .Cats
+                            .Select(c => new
+                            {
+                                c.Id,
+                                c.Name
+                            })
+                            .ToList();
+
+                        await context.Response.WriteAsync("<ul>");
+                        await context.Response.WriteAsync(@"
+                              <form action =""/cat/add"">
+                              <input type =""submit"" value=""Add Cat""/>        
+                              </form>");
+
+                        foreach (var cat in catLinks)
+                        {
+                            await context.Response.WriteAsync($@"<li><a href=""/cat/{cat.Id}>{cat.Name}</a></li>");
+                        }
+
+                    });
+                });
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("404 Page Was Not Found :/");
             });
         }
     }
