6442b7c87e4b99ec9e1f27bc3e09b76eca90f607
VladislavPenchev <vgpenchev@gmail.com>
2018-01-29 16:44:36 +0200

    Implement database

--- CarDealer.Web/CarDealer.Data/CarDealerDbContext.cs
+++ CarDealer.Web/CarDealer.Data/CarDealerDbContext.cs
@@ -11,9 +11,55 @@
         {
         }
 
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Part> Parts { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder
+                .Entity<PartCar>()
+                .HasKey(pc => new { pc.PartId, pc.CarId });
+
+            builder
+                .Entity<PartCar>()
+                .HasOne(pc => pc.Car)
+                .WithMany(c => c.Parts)
+                .HasForeignKey(pc => pc.CarId);
+
+            builder
+                .Entity<PartCar>()
+                .HasOne(pc => pc.Part)
+                .WithMany(p => p.Cars)
+                .HasForeignKey(pc => pc.PartId);
+
+
+            builder
+                .Entity<Sale>()
+                .HasOne(s => s.Car)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(s => s.CarId);
+
+            builder
+                .Entity<Sale>()
+                .HasOne(s => s.Customer)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(s => s.CustomerId);
+
+            builder
+                .Entity<Supplier>()
+                .HasMany(s => s.Parts)
+                .WithOne(p => p.Supplier)
+                .HasForeignKey(p => p.SupplierId);
+
             base.OnModelCreating(builder);
+
         }
     }
 }
--- CarDealer.Web/CarDealer.Data/Migrations/20180129143500_InitialTables.Designer.cs
+++ CarDealer.Web/CarDealer.Data/Migrations/20180129143500_InitialTables.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CarDealer.Data.Migrations
 {
     [DbContext(typeof(CarDealerDbContext))]
-    partial class CarDealerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180129143500_InitialTables")]
+    partial class InitialTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
