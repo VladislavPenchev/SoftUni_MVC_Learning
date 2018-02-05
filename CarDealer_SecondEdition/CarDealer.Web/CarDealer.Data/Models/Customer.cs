da1fd7089ffaf0a3bcf456d61190d45e898788f5
VladislavPenchev <vgpenchev@gmail.com>
2018-02-05 12:28:25 +0200

    implement db

---
+++ CarDealer_SecondEdition/CarDealer.Web/CarDealer.Data/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace CarDealer.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        public bool isYoungDriver { get; set; }
+
+        public List<Sale> Sales { get; set; } = new List<Sale>();
+    }
+}
