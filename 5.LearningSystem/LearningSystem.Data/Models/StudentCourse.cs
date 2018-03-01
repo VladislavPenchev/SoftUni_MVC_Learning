c5a276bfdcfabbd839908883879fc1dbb7469f7e
VladislavPenchev <vgpenchev@gmail.com>
2018-03-01 17:27:09 +0200

    5.LearningSystem => make model & migrations

---
+++ 5.LearningSystem/LearningSystem.Data/Models/StudentCourse.cs
@@ -0,0 +1,15 @@
+﻿namespace LearningSystem.Data.Models
+{
+    using LearningSystem.Web.Models;
+
+    public class StudentCourse
+    {
+        public string StudentId { get; set; }
+
+        public User Student { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
